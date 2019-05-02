7b5126ee35081f0f919dcc268497190ce51a9a1e
shlomo rabinoich <shlomor25@gmalil.com>
2019-05-02 01:05:26 -0700

    add more files

---
+++ cmake-build-debug/CMakeFiles/OS3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS3.dir/ex31.c.o"
+  "CMakeFiles/OS3.dir/ex32.c.o"
+  "OS3.pdb"
+  "OS3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OS3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
